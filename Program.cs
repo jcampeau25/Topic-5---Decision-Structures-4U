7d88b0f7ed3b2e485f2b261723dbc5ae9ef40df4
Joshua Campeau <339729675@GoTVDSB.ca>
2026-02-11 12:53:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5___Decision_Structures_4U
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
